9dd22bba6b62be79ba596688d01d6793fc02d33d
Rishav Aich <aich.1@iitj.ac.in>
2025-01-11 22:22:17 +0530

    added the deployed url to swagger API

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://crypto-tracker-three-livid.vercel.app/',
         description: 'Development server',
       },
     ],
